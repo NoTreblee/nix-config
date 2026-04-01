02aa5f5ba8fba33bb02725e4ac5ea4e1e42883b1
NoTreblee <fiercestword@gmail.com>
2026-04-01 18:24:06 +0200

    removed nixondroid support

--- home/szlacroxx/home.nix
+++ home/szlacroxx/home.nix
@@ -1,23 +1,17 @@
 { config, pkgs, lib, ... }:
 
-let
-  isDroid = builtins.pathExists /data/data/com.termux.nix;
-in
 {
   imports = [
-    (if isDroid then ./modules/zsh-droid.nix else ./modules/zsh.nix)
+    ./modules/cups.nix
+    ./modules/ssh.nix
     ./modules/git.nix
     ./modules/packages.nix
-  ] ++ lib.optionals (!isDroid) [
     ../../modules/home/gnome.nix
     ../../modules/home/niri.nix
   ];
 
-  home.username     = "szlacroxx";
-  home.homeDirectory =
-    if isDroid
-    then "/data/data/com.termux.nix/files/home"
-    else "/home/szlacroxx";
+  home.username      = "szlacroxx";
+  home.homeDirectory = "/home/szlacroxx";
 
   programs.home-manager.enable = true;
   home.stateVersion = "24.11";
